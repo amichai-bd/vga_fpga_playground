861a639edb87f06cff6cfadb5aaa3d34d02b2aee
amichaibd <amichaibd@gmail.com>
2022-05-28 23:25:27 +0300

    pll 25Mhz full 480*640 display decode

--- vga_playground/sv/sync_gen.sv
+++ vga_playground/sv/sync_gen.sv
@@ -1,6 +1,6 @@
 `include "definitions.sv"
 module sync_gen(
-        input  logic        clk,
+        input  logic        CLK_25,
         input  logic        Reset,
         output logic        vga_h_sync,
         output logic        vga_v_sync,
@@ -15,28 +15,27 @@ module sync_gen(
 //VGA @ 640x480 resolution @ 60Hz requires a pixel clock of 25.175Mhz.
 //The Kiwi has an Onboard 50Mhz oscillator, we can divide it and get a 25Mhz clock.
 //It's not the exact frequency required for the VGA standard, but it works fine and it saves us the use of a PLL.
-logic clkdiv;
 logic h_sync, v_sync;
 logic next_h_sync, next_v_sync;
 logic CounterXmaxed, CounterYmaxed;
 logic NextinDisplayArea;
-//gen Clock 25Mhz
-`RST_MSFF(clkdiv, !clkdiv, clk, Reset)
-assign CounterXmaxed = (CounterX == 800); // 16 + 48 + 96 + 640
-assign CounterYmaxed = (CounterY == 525); // 10 + 2 + 33 + 480
+
+//Maxed x y
+assign CounterXmaxed = (CounterX == 800) || Reset; // 16 + 48 + 96 + 640
+assign CounterYmaxed = (CounterY == 525) || Reset; // 10 + 2 + 33 + 480
 
 //x and y counters
-`RST_MSFF   (CounterX, (CounterX+1'b1), clkdiv, CounterXmaxed)
-`EN_RST_MSFF(CounterY, (CounterY+1'b1), clkdiv, CounterXmaxed, (CounterXmaxed && CounterYmaxed) )
+`RST_MSFF   (CounterX, (CounterX+1'b1), CLK_25, CounterXmaxed)
+`EN_RST_MSFF(CounterY, (CounterY+1'b1), CLK_25, CounterXmaxed, (CounterXmaxed && CounterYmaxed) )
 
 assign next_h_sync = (CounterX >= (640 + 16) && (CounterX < (640 + 16 + 96)));   // active for 96 clocks
 assign next_v_sync = (CounterY >= (480 + 10) && (CounterY < (480 + 10 + 2)));   // active for 2 clocks
-`MSFF(h_sync, next_h_sync, clkdiv)
-`MSFF(v_sync, next_v_sync, clkdiv)
+`MSFF(h_sync, next_h_sync, CLK_25)
+`MSFF(v_sync, next_v_sync, CLK_25)
 
 //Indication that we must not send Data in VGA RGB
 assign NextinDisplayArea = ((CounterX < 640) && (CounterY < 480));
-`MSFF(inDisplayArea, NextinDisplayArea, clkdiv)
+`MSFF(inDisplayArea, NextinDisplayArea, CLK_25)
 
 assign vga_h_sync = ~h_sync;
 assign vga_v_sync = ~v_sync;
--- vga_playground/sv/top.sv
+++ vga_playground/sv/top.sv
@@ -7,6 +7,9 @@ module top(
         output logic [6:0]  HEX0,
         output logic [6:0]  HEX1,
         output logic [6:0]  HEX2,
+        output logic [6:0]  HEX3,
+        output logic [6:0]  HEX4,
+        output logic [6:0]  HEX5,
         output logic [9:0]  LED,
 
         output logic [3:0]  RED,
@@ -16,48 +19,85 @@ module top(
         output logic        v_sync
     );
 
-logic [9:0]             pixel_x;
-logic [9:0]             pixel_y;
-logic                   reset;
-logic                   inDisplayArea;
+logic [9:0]  pixel_x;
+logic [9:0]  pixel_y;
+logic        Reset;
+logic        CLK_25;
+logic        inDisplayArea;
+logic        next_h_sync;
+logic        next_v_sync;
+logic [3:0]  NextRED;
+logic [3:0]  NextGREEN;
+logic [3:0]  NextBLUE;
+logic        CurentPixel;
  
-assign reset = ~BUTTON[0];
-
-logic [15:0] [0:127] Display;
-always_comb begin   //  0 H        1 E          2 L         3 L         4 O         5 -         6 W         7 O         8 R         9 L         10 D        11 !         12          13          14          15
-    Display[0] = {8'b00000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000,8'b0000000,8'b000000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000};
-    Display[1] = {8'b11001100,8'b11111100,8'b11000000,8'b11000000,8'b01111000,8'b0000000,8'b110001100,8'b01111000,8'b11111000,8'b11000000,8'b11110000,8'b11000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000};
-    Display[2] = {8'b11001100,8'b11000000,8'b11000000,8'b11000000,8'b11001100,8'b0000000,8'b110001100,8'b11001100,8'b11001100,8'b11000000,8'b11001000,8'b11000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000};
-    Display[3] = {8'b11111100,8'b11111000,8'b11000000,8'b11000000,8'b11001100,8'b1111100,8'b110001100,8'b11001100,8'b11111000,8'b11000000,8'b11000100,8'b11000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000};
-    Display[4] = {8'b11001100,8'b11000000,8'b11000000,8'b11000000,8'b11001100,8'b1111100,8'b110101100,8'b11001100,8'b11010000,8'b11000000,8'b11000100,8'b11000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000};
-    Display[5] = {8'b11001100,8'b11000000,8'b11000000,8'b11000000,8'b11001100,8'b0000000,8'b111011100,8'b11001100,8'b11001000,8'b11000000,8'b11001000,8'b00000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000};
-    Display[6] = {8'b11001100,8'b11111100,8'b11111100,8'b11111100,8'b01111000,8'b0000000,8'b110001100,8'b01111000,8'b11000100,8'b11111100,8'b11110000,8'b11000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000};
-    Display[7] = {8'b00000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000,8'b0000000,8'b000000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000};
-    //
-    Display[8]  = '0;
-    Display[9]  = '0;
-    Display[10] = '0;
-    Display[11] = '0;
-    Display[12] = '0;
-    Display[13] = '0;
-    Display[14] = '0;
-    Display[15] = '0;
+assign Reset = ~BUTTON[0];
+//    Y(480)   X(640)
+logic [479:0] [0:639] Display;
+always_comb begin   
+    Display     = '0;
+                    //         H          E          L            L           O          -           W           O           R           L           D           !        
+    Display[100] = {8'b0,8'b00000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000,512'b0};
+    Display[101] = {8'b0,8'b11001100,8'b11111100,8'b11000000,8'b11000000,8'b01111000,8'b00000000,8'b11000110,8'b01111000,8'b11111000,8'b11000000,8'b11110000,8'b11000000,8'b00000000,8'b00000000,8'b00000000,512'b0};
+    Display[102] = {8'b0,8'b11001100,8'b11000000,8'b11000000,8'b11000000,8'b11001100,8'b00000000,8'b11000110,8'b11001100,8'b11001100,8'b11000000,8'b11001000,8'b11000000,8'b00000000,8'b00000000,8'b00000000,512'b0};
+    Display[103] = {8'b0,8'b11111100,8'b11111000,8'b11000000,8'b11000000,8'b11001100,8'b11111100,8'b11000110,8'b11001100,8'b11111000,8'b11000000,8'b11000100,8'b11000000,8'b00000000,8'b00000000,8'b00000000,512'b0};
+    Display[104] = {8'b0,8'b11001100,8'b11000000,8'b11000000,8'b11000000,8'b11001100,8'b11111100,8'b11010110,8'b11001100,8'b11010000,8'b11000000,8'b11000100,8'b11000000,8'b00000000,8'b00000000,8'b00000000,512'b0};
+    Display[105] = {8'b0,8'b11001100,8'b11000000,8'b11000000,8'b11000000,8'b11001100,8'b00000000,8'b11101110,8'b11001100,8'b11011000,8'b11000000,8'b11001000,8'b00000000,8'b00000000,8'b00000000,8'b00000000,512'b0};
+    Display[106] = {8'b0,8'b11001100,8'b11111100,8'b11111100,8'b11111100,8'b01111000,8'b00000000,8'b11000110,8'b01111000,8'b11001100,8'b11111100,8'b11110000,8'b11000000,8'b00000000,8'b00000000,8'b00000000,512'b0};
+    Display[107] = {8'b0,8'b00000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000,8'b00000000,512'b0};
 end
+
+//ram2port ram2port (
+//	.clock     (CLK_25),
+//	.data      (),
+//	.rdaddress (),
+//	.wraddress (),
+//	.wren      (),
+//	.q         ()
+//);
+
+assign CurentPixel = Display[pixel_y[8:0]][pixel_x[9:0]];
+
+//Only One or the other
+`define FPGA_ON    
+//`define SIMULATION_ON
+
+
+//gen Clock 25Mhz
+`ifdef SIMULATION_ON
+    `RST_MSFF(CLK_25, !CLK_25, CLK_50, Reset)
+`elsif FPGA_ON
+pll_2 pll_2 (
+    .inclk0 (CLK_50),    // input
+    .c0     (CLK_25)     // output
+); 
+`endif
+
 sync_gen sync_inst(
-.clk            (CLK_50),
-.Reset          (reset),
-.vga_h_sync     (h_sync),
-.vga_v_sync     (v_sync),
-.CounterX       (pixel_x),
-.CounterY       (pixel_y),
-.inDisplayArea  (inDisplayArea)
+.CLK_25         (CLK_25),       //input
+.Reset          (Reset),        //input
+.vga_h_sync     (next_h_sync),  //output
+.vga_v_sync     (next_v_sync),  //output
+.CounterX       (pixel_x),      //output
+.CounterY       (pixel_y),      //output
+.inDisplayArea  (inDisplayArea) //output
 );
 
-assign RED      =  (inDisplayArea && SW[0]) ? {4{Display[pixel_y[3:0]][pixel_x[6:0]]}} : '0;
-assign GREEN    =  (inDisplayArea && SW[1]) ? {4{Display[pixel_y[3:0]][pixel_x[6:0]]}} : '0;
-assign BLUE     =  (inDisplayArea && SW[2]) ? {4{Display[pixel_y[3:0]][pixel_x[6:0]]}} : '0;
+assign NextRED   = (inDisplayArea && SW[0]) ? {4{CurentPixel}} : '0;
+assign NextGREEN = (inDisplayArea && SW[1]) ? {4{CurentPixel}} : '0;
+assign NextBLUE  = (inDisplayArea && SW[2]) ? {4{CurentPixel}} : '0;
+
+`MSFF(RED    , NextRED     , CLK_25)
+`MSFF(GREEN  , NextGREEN   , CLK_25)
+`MSFF(BLUE   , NextBLUE    , CLK_25)
+`MSFF(h_sync , next_h_sync , CLK_25)
+`MSFF(v_sync , next_v_sync , CLK_25)
 
 
+
+
+
+//Other FPGA Output
 logic [6:0] HEX; 
 seg7 seg7(
         .input_dig  (SW),
@@ -66,10 +106,13 @@ seg7 seg7(
 assign HEX0 = HEX;
 assign HEX1 = HEX;
 assign HEX2 = HEX;
+assign HEX3 = HEX;
+assign HEX4 = HEX;
+assign HEX5 = HEX;
 
 always_comb begin
-   LED = '0;
-   LED[SW[2:0]] = 1'b1;
+    LED = '0;
+    LED[SW[2:0]] = 1'b1;
 end
 
 endmodule
